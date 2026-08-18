9b17c4907a38f5a64de20cc17ec68f2f91ca2178
Luca Ferrone <lucaferrone3@gmail.com>
2026-08-18 16:43:40 +0000

    feat: enhance testgetpid to validate getpid() functionality

--- src/userland/testbin/testgetpid/testgetpid.c
+++ src/userland/testbin/testgetpid/testgetpid.c
@@ -3,6 +3,22 @@
 
 int main(void)
 {
-    printf("PID: %d\n", getpid());
+    pid_t pid1, pid2, pid3;
+
+    pid1 = getpid();
+    pid2 = getpid();
+    pid3 = getpid();
+
+    printf("PID 1: %d\n", pid1);
+    printf("PID 2: %d\n", pid2);
+    
+    printf("PID 3: %d\n", pid3);
+
+    if (pid1 == pid2 && pid2 == pid3) {
+        printf("getpid() test: PASS\n");
+    } else {
+        printf("getpid() test: FAIL\n");
+    }
+
     return 0;
 }
