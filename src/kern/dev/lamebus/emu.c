a149c8ccd1cce75313851750aec744632ac90518
devgfe <gabriele.ferrero.2003@gmail.com>
2026-09-01 23:56:49 +0000

    refactor(emufs): simplify child path construction in lookup and create

--- src/kern/dev/lamebus/emu.c
+++ src/kern/dev/lamebus/emu.c
@@ -774,25 +774,14 @@ emufs_creat(struct vnode *dir, const char *name, bool excl, mode_t mode,
 
 	dirlen = strlen(ev->ev_path);
 	namelen = strlen(name);
-	if (dirlen == 0) {
-		if (1 + namelen + 1 > PATH_MAX) { // layout: "/" + name + NUL
-			kfree(childpath);
-			emu_close(ev->ev_emu, handle);
-			return ENAMETOOLONG;
-		}
-		strcpy(childpath, "/");
-		strcat(childpath, name);
+	if (dirlen + 1 + namelen + 1 > PATH_MAX) { // layout: ev_path + "/" + name + NUL
+		kfree(childpath);
+		emu_close(ev->ev_emu, handle);
+		return ENAMETOOLONG;
 	}
-	else {
-		if (dirlen + 1 + namelen + 1 > PATH_MAX) { // layout: ev_path + "/" + name + NUL
-			kfree(childpath);
-			emu_close(ev->ev_emu, handle);
-			return ENAMETOOLONG;
-		}
-		strcpy(childpath, ev->ev_path);
-		strcat(childpath, "/");
-		strcat(childpath, name);
-	}
+	strcpy(childpath, ev->ev_path);
+	strcat(childpath, "/");
+	strcat(childpath, name);
 
 	result = emufs_loadvnode(ef, handle, isdir, childpath, &newguy);
 	kfree(childpath);
@@ -842,25 +831,14 @@ emufs_lookup(struct vnode *dir, char *pathname, struct vnode **ret)
 
 	dirlen = strlen(ev->ev_path);
 	pathlen = strlen(pathname);
-	if (dirlen == 0) {
-		if (1 + pathlen + 1 > PATH_MAX) { // layout: "/" + name + NUL
-			kfree(childpath);
-			emu_close(ev->ev_emu, handle);
-			return ENAMETOOLONG;
-		}
-		strcpy(childpath, "/");
-		strcat(childpath, pathname);
+	if (dirlen + 1 + pathlen + 1 > PATH_MAX) { // layout: ev_path + "/" + name + NUL
+		kfree(childpath);
+		emu_close(ev->ev_emu, handle);
+		return ENAMETOOLONG;
 	}
-	else {
-		if (dirlen + 1 + pathlen + 1 > PATH_MAX) { // layout: ev_path + "/" + name + NUL
-			kfree(childpath);
-			emu_close(ev->ev_emu, handle);
-			return ENAMETOOLONG;
-		}
-		strcpy(childpath, ev->ev_path);
-		strcat(childpath, "/");
-		strcat(childpath, pathname);
-	}
+	strcpy(childpath, ev->ev_path);
+	strcat(childpath, "/");
+	strcat(childpath, pathname);
 
 	result = emufs_loadvnode(ef, handle, isdir, childpath, &newguy);
 	kfree(childpath);
